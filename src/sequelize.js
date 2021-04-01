3af5fa38c1998fbead11ffa057825a4190c97e03
BuildTools <semgo@gmail.com>
2021-04-01 20:04:50 +0300

    make it better

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,7 +1,9 @@
 import pkg from 'sequelize';
 const { Sequelize, DataTypes } = pkg;
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres"
+});
 
 sequelize.define("User", {
     user_id: {
