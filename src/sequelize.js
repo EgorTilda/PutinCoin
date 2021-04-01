6446f4806f98216942ef9bb7b9b9e7c63abb19b5
BuildTools <semgo@gmail.com>
2021-04-01 20:14:33 +0300

    make it better

--- src/sequelize.js
+++ src/sequelize.js
@@ -2,7 +2,7 @@ import pkg from 'sequelize';
 const { Sequelize, DataTypes } = pkg;
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    ssl: false,
+    ssl: true,
     dialect: "postgres"
 });
 
